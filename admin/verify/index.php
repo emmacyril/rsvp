6f7d890a2c107b2bed9d839e986dca74af068a18
Akash Jaiswal <akashjas1996@gmail.com>
2019-10-29 00:44:31 +0530

    target

--- admin/verify/index.php
+++ admin/verify/index.php
@@ -138,7 +138,7 @@ td, th {
                                             <?php echo $row_verify['doc_no'] ?>
                                         </td>
                                         <td>
-                                            <a href="display/view.php?fl=<?php echo $row_verify['doc_name'] ?>"> <?php echo $row_verify['doc_name'] ?> </a>
+                                            <a target="_blank" href="http://rsvpsalenmarketing.com/kyc/<?php echo $row_verify['doc_name'] ?>"> <?php echo $row_verify['doc_name'] ?> </a>
                                         </td>
                                     </tr>
                                 <?php } ?>
