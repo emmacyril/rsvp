1471d9627acf24ae2dce77b83928d0304d16b006
Akash Jaiswal <akashjas1996@gmail.com>
2019-11-03 12:11:15 +0530

    db

--- php-includes/connect_l.php
+++ php-includes/connect_l.php
@@ -8,3 +8,7 @@
 		echo 'connect to database failed';
 	}
 ?>
+
+
+
+
--- php-includes/connect_l_useless.php
+++ php-includes/connect_l_useless.php
@@ -1,12 +1,8 @@
-
-
-
-
 <?php
-	$db_host = "localhost";
-	$db_user = "u502039025_pvsr";
-	$db_pass = "4*CAKd1NY4JI";
-	$db_name = "u502039025_rsvp";
+	$db_host = "127.0.0.1";
+	$db_user = "root";
+	$db_pass = "password";
+	$db_name = "rsvp";
 	$con =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 	if(mysqli_connect_error()){
 		echo 'connect to database failed';
